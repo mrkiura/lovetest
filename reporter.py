bea9707954a2b42a24f6d686ccdf0a5778e1ba8c
Alex <kiuraalex@gmail.com>
2023-12-26 12:56:04 +0300

    Report test results (#11)
    * Test reporter
    
    * Add reporter

---
+++ reporter.py
@@ -0,0 +1,41 @@
+from collections import Counter
+from typing import Dict
+
+
+def print_results(report: Dict) -> None:
+    print("Running Tests...\n")
+
+    summary = Counter()
+    for test_file, (results, counter, errors, failures) in report.items():
+        print("\n" + "=" * 70)
+        print(f"{test_file}")
+        print("=" * 70)
+        summary["PASS"] += counter["PASS"]
+        summary["FAIL"] += counter["FAIL"]
+        summary["ERROR"] += counter["ERROR"]
+        summary["ELAPSED"] += counter["ELAPSED"]
+        for test_name, test_result in results.items():
+            summary["COUNT"] += 1
+            if "FAIL" in test_result:
+                print(f"{test_name} ... FAIL")
+            elif "ERROR" in test_result:
+                print(f"{test_name} ... ERROR")
+            elif "PASS" in test_result:
+                print(f"{test_name} ... OK")
+
+        for test_name, error_message in errors.items():
+            print(
+                f"ERROR: {test_name}\n----------------------------------------------------------------------"
+            )
+            print(f"{error_message}\n")
+
+        for test_name, error_message in failures.items():
+            print(
+                f"FAIL: {test_name}\n----------------------------------------------------------------------"
+            )
+            print(f"{error_message}\n")
+
+    print(f"\nRan {summary['COUNT']} tests\n")
+    print("PASS (pass=" + str(summary["PASS"]) + ")")
+    print("FAIL (fail=" + str(summary["FAIL"]) + ")")
+    print("ERROR (error=" + str(summary["ERROR"]) + ")")
---
+++ tests/test_reporter.py
@@ -0,0 +1,65 @@
+from collections import Counter
+import io
+import sys
+
+from reporter import print_results
+
+
+def capture_print_output(func, *args, **kwargs):
+    original_stdout = sys.stdout
+    sys.stdout = io.StringIO()
+    try:
+        func(*args, **kwargs)
+        return sys.stdout.getvalue()
+    finally:
+        sys.stdout = original_stdout
+
+
+def test_empty_report():
+    report = {}
+    result = capture_print_output(print_results, report)
+    assert "Ran 0 tests" in result
+
+
+def test_all_passing():
+    report = {
+        'test_file_01': (
+            {'test_pass_01': 'PASS'},
+            Counter({'PASS': 1, 'FAIL': 0, 'ERROR': 0, 'ELAPSED': 0.1}),
+            {},
+            {}
+        )
+    }
+    result = capture_print_output(print_results, report)
+    assert "test_pass_01 ... OK" in result
+
+
+def test_mixed_results():
+    report = {
+        'test_file_01': (
+            {'test_pass_01': 'PASS', 'test_fail_01': 'FAIL', 'test_error_01': 'ERROR'},
+            Counter({'PASS': 1, 'FAIL': 1, 'ERROR': 1, 'ELAPSED': 0.3}),
+            {'test_error_01': 'An error occurred.'},
+            {'test_fail_01': 'AssertionError'}
+        )
+    }
+    result = capture_print_output(print_results, report)
+    assert "test_fail_01 ... FAIL" in result
+    assert "test_error_01 ... ERROR" in result
+    assert "ERROR: test_error_01" in result
+    assert "FAIL: test_fail_01" in result
+
+
+def test_no_passing_tests():
+    report = {
+        'test_file_01': (
+            {'test_fail_01': 'FAIL', 'test_error_01': 'ERROR'},
+            Counter({'PASS': 0, 'FAIL': 1, 'ERROR': 1, 'ELAPSED': 0.2}),
+            {'test_error_01': 'An error occurred.'},
+            {'test_fail_01': 'AssertionError'}
+        )
+    }
+    result = capture_print_output(print_results, report)
+    assert "test_fail_01 ... FAIL" in result
+    assert "test_error_01 ... ERROR" in result
+    assert "test_pass_01 ... OK" not in result
